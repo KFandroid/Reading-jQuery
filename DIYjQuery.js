4a007e4c58824246641083a674d89e1e2dc2cb39
lu_hb <lu_hb@centit.com>
2017-11-13 10:58:37 +0800

    1、diy一个jQuery

--- DIYjQuery.js
+++ DIYjQuery.js
@@ -3,14 +3,57 @@
  */
 (function () {
     var _jQuery = window.jQuery,
-            _$ = window.$;
+            _$ = window.$;//把之前的jQuery保存起来，需要noConflict时把这两个变量吐出去。
 
     var version = "0.0.1",
         jQuery = function(selector){
-        console.log(document.querySelector(selector));
+            return new jQuery.fn.init(selector);
         };
 
-    window.jQuery = window.$ = function (selector) {
-        return new jQuery(selector);
+    jQuery.prototype = {
+        init: function (selector) {
+          if(!selector){
+
+          }else{
+              var elem = document.querySelector(selector);
+              if(elem){
+                  this[0] = elem;
+                  this.length = 1;
+              }
+          }
+            return this;
+        },
+        setBackground:function (background) {
+            this[0].style.background = background;
+            return this;
+        },
+        version:version,
+        name:function () {
+            console.log(this);
+        },
+
     }
+
+    jQuery.noConflict = function(deep) {
+        if(window.$ === $){
+            window.$ = _$;
+        }
+
+        if(deep && window.jQuery === jQuery){
+            window.jQuery === _jQuery;
+        }
+
+        return jQuery;
+    }
+
+    jQuery.fn = jQuery.prototype;
+
+    jQuery.fn.init.prototype = jQuery.prototype;
+
+
+    jQuery.extend = jQuery.fn.extend = function () {
+        console.log(this)
+    }
+
+    window.jQuery = window.$ = jQuery;
 })();
